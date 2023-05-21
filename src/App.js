7b9ade6c7fa3f7e10073674738caa30e6983c919
Luciano Montilla <unonueve95@gmail.com>
2023-05-21 19:08:03 -0300

    Ejercicio 5. Trabajo con Formik y Yup

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import CompA from './ejercicio-01/compA';
 import Clock from './ejercicio-02/classtofunction';
 import ListComponent from './ejercicio-03/list';
 import ConditionalColors from './ejercicio-04/conditionalColors';
+import TaskListComponent from './ejercicio-05/task_list';
 
 function App() {
   return (
@@ -18,7 +19,10 @@ function App() {
         {/* <ListComponent></ListComponent> */}
 
         {/* Ejercicio 4 */}
-        <ConditionalColors></ConditionalColors>
+        {/* <ConditionalColors></ConditionalColors> */}
+
+        {/* Ejercicio 5 */}
+        <TaskListComponent></TaskListComponent>
     </div>
   );
 }
